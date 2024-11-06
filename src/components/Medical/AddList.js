367ce2db5f520f00bb08fc831b8b55cdb366814f
DongJun-Choi <akxmenltls@naver.com>
2024-11-06 11:26:35 +0900

    dong

--- src/components/Medical/AddList.js
+++ src/components/Medical/AddList.js
@@ -19,7 +19,7 @@ const AddList = () => {
                 // postSearchPatient API 호출
                 const data = await postSearchPatient(inputText);
                 console.log('Check response', data)
-
+                
                 // 서버 응답이 성공적일 경우 리스트에 항목 추가
                 if (data && data.success) { // 성공 여부에 따라 조건 변경 가능
                     setList([...list, {
@@ -27,7 +27,7 @@ const AddList = () => {
                         username: data.name
                     }]);
                     setInputText(''); // 텍스트 필드 초기화
-                } else {
+                } else { //else if문을 통해 data.status의 메세지에 따라 변경
                     alert('환자를 찾을 수 없습니다.');
                 }
             } catch (error) {
@@ -51,7 +51,7 @@ const AddList = () => {
                 {list.map((item, index) => (
                     <li key={index}>
                         {item.userid} - {item.username}
-                    </li> // 리스트 항목의 userid 출력
+                    </li> // 리스트 항목의 userid, username 출력
                 ))}
             </ul>
         </div>
