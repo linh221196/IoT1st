abc5cef9573a1b8a653651035ccc89e87a884799
DongJun-Choi <akxmenltls@naver.com>
2024-11-11 15:49:46 +0900

    dong

--- src/components/User/UserCallVolunteer.js
+++ src/components/User/UserCallVolunteer.js
@@ -15,6 +15,8 @@ const UserCallVolunteer = () => {
             if (data.status === "TokenInvalid") {
                 alert("유효하지 않은 토큰");
                 navigate('/');
+            } else {
+                console.log('토큰사용자 인증완료')
             }
         } catch (error) {
             alert("오류가 발생했습니다. 다시 시도해 주세요.");
