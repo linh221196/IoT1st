e22009b1ca13e95bcea51c4b967b9f5f0454cda5
DongJun-Choi <akxmenltls@naver.com>
2024-11-19 18:16:36 +0900

    dong

--- src/components/Medical/HeaderBar.js
+++ src/components/Medical/HeaderBar.js
@@ -1,78 +1,28 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
+import './HeaderBar.scss'; // SCSS 파일을 연결해 스타일을 분리
 
 const HeaderBar = () => {
-    const userInfo = useSelector(state => state.user.account)
-    console.log("userInfo:", userInfo);
-    console.log("userInfo:", userInfo);
-    console.log("userInfo:", userInfo);
-    console.log("userInfo:", userInfo);
-
+    const userInfo = useSelector(state => state.user.account);
 
     return (
-        <div
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                backgroundColor: '#333',
-                color: '#fff',
-                padding: '10px 20px',
-                position: 'fixed',
-                width: '100%',
-                top: 0,
-                zIndex: 1000,
-            }}
-        >
+        <header className="header-bar">
             {/* 로고 */}
-            <div
-                style={{
-                    fontSize: '24px',
-                    fontWeight: 'bold',
-                    cursor: 'pointer',
-                }}
-            >
-                케어 메이트
-            </div>
+            <div className="header-logo">케어 메이트</div>
 
             {/* 메뉴 링크 */}
-            <nav style={{ display: 'flex', gap: '20px' }}>
-                <a
-                    href="/MedicalHome"
-                    style={{
-                        color: '#fff',
-                        textDecoration: 'none',
-                        fontSize: '16px',
-                        fontWeight: '500',
-                    }}
-                >
-                    MedicalHome
-                </a>
-                <a
-                    href="/MedicalAdd"
-                    style={{
-                        color: '#fff',
-                        textDecoration: 'none',
-                        fontSize: '16px',
-                        fontWeight: '500',
-                    }}
-                >
-                    MedicalAddPatient
-                </a>
+            <nav className="header-nav">
+                <a href="/MedicalHome" className="header-link">MedicalHome</a>
+                <a href="/MedicalAdd" className="header-link">MedicalAddPatient</a>
             </nav>
 
-            {/* 오른쪽 글귀 */}
-            <div
-                style={{
-                    fontSize: '16px',
-                    fontStyle: 'italic',
-                }}
-            >
+            {/* 오른쪽 정보 */}
+            <div className="header-user-info">
                 {userInfo && userInfo.role === "Medical"
                     ? `${userInfo.name}님 (${userInfo.email}) 환영합니다.`
                     : "로그인 정보를 확인할 수 없습니다."}
             </div>
-        </div>
+        </header>
     );
 };
 
