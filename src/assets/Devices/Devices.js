8472993884b02dc6d349ede6f0f9ee5ac013691c
gyuhyeok <jkh7562@gmail.com>
2024-12-02 14:37:16 +0900

    수정

--- src/assets/Devices/Devices.js
+++ src/assets/Devices/Devices.js
@@ -27,21 +27,23 @@ const Devices =
         img: AirFlow,
         des: "Air FLow 호흡센서",
         imoticon: airflowimoticon,
-        link: "https://www.youtube.com/watch?v=KlGaa6K0BsI&t=348s"
+        link: "https://youtube.com/watch?v=OV4k2_rI1kY"
     },
     {
         id: 2,
         name: "Body Temp 체온센서",
         img: BodyTemp,
         des: "Body Temp 체온센서",
-        imoticon: bodytempimoticon
+        imoticon: bodytempimoticon,
+        link: "https://youtube.com/shorts/rARYU-K6Pvg?feature=share"
     },
     {
         id: 3,
         name: "NIBP 혈압 측정계",
         img: NIBP,
         des: "혈압 측정계",
-        imoticon: nibpimoticon
+        imoticon: nibpimoticon,
+        link: "https://youtu.be/OZ8qV-8iLY4"
     },
     // {
     //     id: 4,
@@ -54,35 +56,40 @@ const Devices =
         name: "SPO2 산소 포화도",
         img: SPO2,
         des: "산소 포화도",
-        imoticon: spo2imoticon
+        imoticon: spo2imoticon,
+        link: "https://youtu.be/6C6QVqHql3E"
     },
     {
         id: 6,
         name: "EOG 안구전도",
         img: EOG,
         des: "안구전도",
-        imoticon: eogimoticon
+        imoticon: eogimoticon,
+        link: "https://youtube.com/shorts/OTFLTgPVpjE"
     },
     {
         id: 7,
         name: "ECG 심전도",
         img: ECG,
         des: "심전도",
-        imoticon: ecgimoticon
+        imoticon: ecgimoticon,
+        link: "https://youtube.com/shorts/02mXnly17CU"
     },
     {
         id: 8,
         name: "EMG 근전도",
         img: EMG,
         des: "근전도",
-        imoticon: emgimoticon
+        imoticon: emgimoticon,
+        link: "https://youtube.com/shorts/02mXnly17CU"
     },
     {
         id: 9,
         name: "GSR Galvanic Skin Response",
         img: GSR,
         des: "GSR Galvanic Skin Response",
-        imoticon: gsrimoticon
+        imoticon: gsrimoticon,
+        link: "https://youtu.be/7AhsPkVztqI"
     },
     ]
 export default Devices
